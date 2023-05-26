a346cf013d67caaee5d627d86561020a1be3b076
mastewal12 <mastewaltakele8@gmail.com>
2023-05-26 02:07:33 -0500

    first commit

---
+++ server/api/answer/answer.router.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { solveQuestion, getAnswerByQuestionId } = require("./answer.controller");
+
+router.post("/", solveQuestion);
+router.get("/:id", getAnswerByQuestionId);
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
